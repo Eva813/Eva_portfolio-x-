4ec49f892fa22371cf8616ac12aacbcf7b60617f
Eva813 <as45986@gmail.com>
2021-01-07 20:49:53 +0800

    localStorage of seats

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -1,38 +1,6 @@
-// var ticketPrice = $("option").val();
 
 
-//選取到該電影
-// $('#movie').on(function () {
-//   var movie = $(this).val();
-//   alert("您擅長的程式語言是 : " + movie);
-// });
-
-// function() {
-//   if ...slected += 1
-// }
-
-//
-// $(".seat").click(function (e) {
-//   var occupied = $(".seat.occupied");
-//   var selectedSeats = document.querySelectorAll('.row .seat.selected');
-
-//   var selectedSeatsCount = selectedSeats.length;
-
-//   $(this).css('background-color', '#6feaf6')
-
-//   $('#count').text(selectedSeatsCount);
-
-// })
-//點選座位時，抓取電影價格，並計算票價, 人數
-// .seat.selected {
-// background - color: #6feaf6;
-//}
-// count.innerText = selectedSeatsCount;
-// total.innerText = selectedSeatsCount * ticketPrice;
-
-// Save selected movie index and price
-//localStorage網頁上的資料儲存在瀏覽器,[[JS] localStorage 筆記](https://medium.com/%E9%A6%AC%E6%A0%BC%E8%95%BE%E7%89%B9%E7%9A%84%E5%86%92%E9%9A%AA%E8%80%85%E6%97%A5%E8%AA%8C/js-localstorage-%E7%AD%86%E8%A8%98-581d432c2d7f),[JS30-Day15-LocalStorage](https://ithelp.ithome.com.tw/articles/10195522)
-
+populateUI();
 
 let ticketPrice = +$('#movie').val();
 //前面多一個+，可以將string轉數字
@@ -40,22 +8,33 @@ let ticketPrice = +$('#movie').val();
 
 
 
+//儲存電影索引以及價格
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+
+}
 
 
 
-//電影選擇事件
+//電影選擇事件movie
 $("#movie").change(function (e) {
   ticketPrice = +e.target.value;
   //console.log(ticketPrice);
+  setMovieData(e.target.selectedIndex, e.target.value);
 
+  //此參數為實際抓取的資料
   updateSelectedCount();
 });
+
+
+
 
 
 //2.總價與計數目
 function updateSelectedCount() {
   var selectedSeats = $('.row .seat.selected');  //被選到的座位
-  var seats = $('.row .seat:not(.occupied)')
+  var seats = $('.row .seat:not(.occupied)');
   var selectedSeatsCount = selectedSeats.length;  //位置的計算就是陣列長度
 
   //*******儲存座位**************** *//
@@ -76,16 +55,44 @@ function updateSelectedCount() {
   $('#total').text(selectedSeatsCount * ticketPrice);
 }
 
+//從localstorage取的資料，以及populateUI
+//[How to Use forEach() to Iterate an Array in JavaScript](https://dmitripavlutin.com/foreach-iterate-array-javascript/)
+//取得選擇的座位
+function populateUI() {
+  var seats = $('.row .seat:not(.occupied)');
+  var selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  //在updateSelectedCount()中，有儲存一個名為selectedSeats的資料，將它轉成arry
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    [...seats].forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  var selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    $('#movie').selectedIndex = selectedMovieIndex;
+  }
+
+  // console.log(selectedMovieIndex)
+}
+
+
 
 
 //1.點擊座位觸發事件：e.target會顯示整個div element
 $(".container").click(function (e) {
+
   //意指要挑選條件為：有seat的class，且沒有occupied的class(也就是選擇到occupied的class是無法點選的)
-  if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
-    e.target.classList.toggle("selected");
+  if ($(e.target).hasClass("seat") && !$(e.target).hasClass("occupied")) {
+    $(e.target).toggleClass("selected");
     //選用toggle可以點擊顯示，再點擊消失
     updateSelectedCount();
   }
+  //e.target.classList.contains("occupied")=> $(e.target).hasClass
 
 });
 
